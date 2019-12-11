d5e2a7e9470630e7aa7b7818959ca8c0565d1811
Tio Rosady <sulistioilhamrosady@gmail.com>
2019-12-11 20:39:15 +0700

    login

---
+++ app/src/main/java/polinema/ac/id/uassewahotel/activities/LoginActivity.java
@@ -0,0 +1,17 @@
+package polinema.ac.id.uassewahotel.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import polinema.ac.id.uassewahotel.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
